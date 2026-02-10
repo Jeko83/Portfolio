9b6f1b7e01c0876d5577d128973909628de1bd21
Jeko <catelan.giacomo@gmail.com>
2026-02-10 21:16:28 +0100

    Refactor Projects component to use a typed Project interface, reverse order for comfort of adding to file and getting the latest one on top.

--- src/Projects.tsx
+++ src/Projects.tsx
@@ -3,12 +3,20 @@ import { HeaderButtons } from "./components/navigation-buttons";
 import projects from "./assets/projects.json";
 import { Footer } from "./Footer";
 
+type Project = {
+  title: string;
+  description: string;
+  skills: string[];
+}
+
+const reversedProjects: Project[] = projects.reverse();
+
 export function Projects() {
   return (
     <div className="relative min-h-screen flex flex-col items-center justify-center px-4 py-16">
       <HeaderButtons />
       <div className="animate-fade-in flex flex-col gap-3 text-center justify-center max-w-lg">
-        {projects.reverse().map((project) => (
+        {reversedProjects.map((project) => (
           <ProjectCard key={project.title} title={project.title} description={project.description} skills={project.skills} />
         ))}
       </div>
